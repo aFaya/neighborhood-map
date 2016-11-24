051aa28e7e3e7ad1f9a3be0689c9b8dbc3680e05
aFa <wadakushiafa@gmail.com>
2016-11-24 20:35:03 +0800

    add feature when clicking item in the list the relative marker should animate.

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@
 (function() {
 
   var map;
+  var marker;
 
   // initial map on the screen.
   function initMap() {
@@ -13,18 +14,28 @@
   }
 
   var markers = [];
-  var locations = [{'title': 'wdf', 'location': {lat: 22.505627, lng: 113.932772}},
-                   {'title': 'Starbucks', 'location': {lat: 22.517494, lng: 113.935624}},
-                   {'title': 'Dayouli Restaurant', 'location': {lat: 22.51841, lng: 114.06416}}
-                   ];
+  var locations = [{'title': 'Window of the World', 'location': {lat: 22.536818, lng: 113.974514}, 'id':0},
+                   {'title': 'Coast City', 'location': {lat: 22.517115, lng: 113.936844}, 'id':1},
+                   {'title': 'Dayouli Restaurant', 'location': {lat: 22.51841, lng: 114.06416}, 'id':2},
+                   {'title': 'Coco Park', 'location': {lat: 22.534206, lng: 114.053701}, 'id': 3},
+                   {'title': 'Da Mei Sha', 'location': {lat: 22.594822, lng: 114.304446}, 'id': 4}];
 
   // represents one spot.
-  var Spot = function(title, location) {
+  var Spot = function(title, location, id) {
     this.title = ko.observable(title);
     this.location = ko.observable(location);
+    this.marker = new google.maps.Marker({
+          position: location,
+          title: title,
+          animation: google.maps.Animation.DROP,
+          id: id
+        });
   };
 
+
   var ViewModel = function() {
+
+    var largeInfoWindow = new google.maps.InfoWindow();
 
     // pop out the infowindow
     function populateInfoWindow(marker, inforwindow) {
@@ -40,7 +51,7 @@
     }
 
     this.spots = ko.observableArray(locations.map(function(spot) {
-      return new Spot(spot.title, spot.location);
+      return new Spot(spot.title, spot.location, spot.id);
     }));
     this.query = ko.observable('');
 
@@ -58,30 +69,46 @@
       return filtered;
     });
 
-    // function resetMarker() {
-    self.resetMarker = ko.computed(function(){
-      var largeInfoWindow = new google.maps.InfoWindow();
+    // list items click eventlistener.
 
-      // if markers exit, remove them and reload.
+    self.resetMarker = ko.computed(function(){
+
+      // if markers exit, remove them.
       if(markers.length !== 0) {
         removeMarker();
       }
 
       // create markers.
       for(var i = 0; i < self.filterItem().length; i++) {
-        var title = self.filterItem()[i].title();
-        var position = self.filterItem()[i].location();
-        var marker = new google.maps.Marker({
-          position: position,
-          title: title,
-          animation: google.maps.Animation.DROP,
-          id: i
-        });
+        // var title = self.filterItem()[i].title();
+        // var position = self.filterItem()[i].location();
+        // marker = new google.maps.Marker({
+        //   position: position,
+        //   title: title,
+        //   animation: google.maps.Animation.DROP,
+        //   id: i
+        // });
+        // marker.addListener('click', function() {
+        //   toggleBounce(this);
+        //   populateInfoWindow(this, largeInfoWindow);
+        // });
+        // markers.push(marker);
+
+        marker = self.filterItem()[i].marker;
         marker.addListener('click', function() {
+          toggleBounce(this);
           populateInfoWindow(this, largeInfoWindow);
         });
         markers.push(marker);
       }
+
+      // function toggleBounce(marker) {
+      //   if(marker.getAnimation() !== null) {
+      //     marker.setAnimation(null);
+      //   }else {
+      //     marker.setAnimation(google.maps.Animation.BOUNCE);
+      //   }
+      // }
 
       // place the markers in the map.
       var bounds = new google.maps.LatLngBounds();
@@ -91,6 +118,20 @@
       }
       map.fitBounds(bounds);
     });
+
+    // click list item, focus on the relative marker.
+    self.focusMarker = function(spot) {
+      toggleBounce(spot.marker);
+      populateInfoWindow(spot.marker, largeInfoWindow);
+    };
+
+    function toggleBounce(marker) {
+      if(marker.getAnimation() !== null) {
+        marker.setAnimation(null);
+      }else {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+      }
+    }
 
     // remove markers from the map.
     function removeMarker() {
